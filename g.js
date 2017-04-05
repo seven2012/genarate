76b66ae1dcae8fb4577ee61c83f3d686860d59bc
july77 <ljlpsy11@gmail.com>
2017-04-05 20:23:15 +0800

    生成器

---
+++ g.js
@@ -0,0 +1,13 @@
+
+//console.assert(gen.tagName === 'BUTTON');
+gen.onclick = function(){
+  space1.innerHTML = word1.value;
+  //console.log(word1.value)
+  space2.innerHTML = word2.value;
+  space3.innerHTML = word3.value;
+  space4.innerHTML = word4.value;
+  space5.innerHTML = word5.value;
+  space6.innerHTML = word6.value;
+  
+}
+ //console.log('done');
